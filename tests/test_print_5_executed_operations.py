3a80ef4e6e04022ce1ee84a4b44c4062e27fbf03
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-21 22:14:43 +0300

    adding file_name to make_path, so you can use any file

--- tests/test_print_5_executed_operations.py
+++ tests/test_print_5_executed_operations.py
@@ -2,21 +2,24 @@ from utils import print_5_executed_operations
 import os
 
 
-def make_path():
+def make_path(file_name):
     if os.name == 'nt':
-        path = '..\\data\\operations.json'
+        path = '..\\data\\' + file_name
     else:
-        path = '../data/operations.json'
+        path = '../data/' + file_name
     return path
 
 
 def test_make_path():
-    path = make_path()
+    path = make_path('operations.json')
     assert print_5_executed_operations.make_path('operations.json') == path
 
 
 def test_reading_file():
     ...
+
+
+
 def test_reading():
     ...
 
