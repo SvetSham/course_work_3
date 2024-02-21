3419a63ff7ea438bb66592299c7622b5c328e50b
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-21 21:39:45 +0300

    takeaway making path to function make_path

--- tests/test_print_5_executed_operations.py
+++ tests/test_print_5_executed_operations.py
@@ -2,13 +2,21 @@ from utils import print_5_executed_operations
 import os
 
 
-def test_make_path():
+def make_path():
     if os.name == 'nt':
-        path = '..\data\operations.json'
+        path = '..\\data\\operations.json'
     else:
         path = '../data/operations.json'
+    return path
+
+
+def test_make_path():
+    path = make_path()
     assert print_5_executed_operations.make_path('operations.json') == path
 
+
+def test_reading_file():
+    ...
 def test_reading():
     ...
 
