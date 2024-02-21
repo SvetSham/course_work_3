77416ec1dee04f40c87c16c1229f3375de5296d2
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-21 20:05:18 +0300

    creating empty functions for tests

---
+++ tests/test_print_5_executed_operations.py
@@ -0,0 +1,16 @@
+from print_5_executed_operations import *
+def test_reading():
+    ...
+
+
+def test_prepare_data():
+    ...
+
+
+def test_printing_data():
+    ...
+
+
+def test_main():
+    ...
+
