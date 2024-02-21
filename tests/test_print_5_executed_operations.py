d55c7a3416b044adedbfe7f4e01ce2dc89352326
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-21 21:18:20 +0300

    create test for making path

--- tests/test_print_5_executed_operations.py
+++ tests/test_print_5_executed_operations.py
@@ -1,4 +1,13 @@
 from print_5_executed_operations import *
+import os
+
+def test_make_path():
+    if os.name == 'nt':
+        path = '..\\data\\operations.json'
+    else:
+        path = '../data/operations.json'
+
+
 def test_reading():
     ...
 
