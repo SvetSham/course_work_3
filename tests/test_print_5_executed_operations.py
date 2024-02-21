d1639337cfd996890028665a2abb4b22b661c9a2
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-21 21:27:49 +0300

    function test_make path

--- tests/test_print_5_executed_operations.py
+++ tests/test_print_5_executed_operations.py
@@ -1,12 +1,13 @@
-from print_5_executed_operations import *
+from utils import print_5_executed_operations
 import os
+
 
 def test_make_path():
     if os.name == 'nt':
-        path = '..\\data\\operations.json'
+        path = '..\data\operations.json'
     else:
         path = '../data/operations.json'
-
+    assert print_5_executed_operations.make_path('operations.json') == path
 
 def test_reading():
     ...
@@ -22,4 +23,3 @@ def test_printing_data():
 
 def test_main():
     ...
-
