4177aac4f824636d38ff6b1586f80c4efd40cabe
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-23 07:49:30 +0300

    adding TEXT_OPERATIONS

--- tests/test_print_5_executed_operations.py
+++ tests/test_print_5_executed_operations.py
@@ -2,6 +2,24 @@ import pytest
 from utils import print_5_executed_operations
 import os
 
+TEXT_OPERATIONS = """[
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }
+]"""
+
 
 @pytest.fixture
 def operations_list_of_dict_fixture():
