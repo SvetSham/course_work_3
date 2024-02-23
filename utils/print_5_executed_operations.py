23bf23f25b8aba6c66c89dcb0eec1725f98d3e67
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-23 12:46:51 +0300

    add getting account name and num to printing_data

--- utils/print_5_executed_operations.py
+++ utils/print_5_executed_operations.py
@@ -82,6 +82,8 @@ def printing_data(five_executed):
         else:
             card_from_num = '???'
             card_from_name = '???'
+        account_to_name = get_from_name(five_executed[i]["to"])
+        account_to_num = get_from_num(five_executed[i]["to"])
 
         print(data, description)
         print(card_from_name, card_from_num, '->')
