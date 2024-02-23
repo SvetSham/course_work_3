59c2342c5cb03d2c5cc5c311af341778cc9b7e1b
Svetlana Shambina <szxd2009@yandex.ru>
2024-02-23 09:30:57 +0300

    realised prepare_data function

--- utils/print_5_executed_operations.py
+++ utils/print_5_executed_operations.py
@@ -22,10 +22,20 @@ def reading_operations_file(filename):
     path = make_path(filename)
     raw_operations = reading_file(path)
     operations = making_list_from_json(raw_operations)
+    prepare_data(operations)
 
 
-def prepare_data():
-    ...
+def prepare_data(operations):
+    five_executed = []
+    i = 0
+    while len(five_executed) < 5:
+        if len(operations) != 0:
+            if operations[i]['state'] == "EXECUTED":
+                five_executed.append(operations[i])
+        i += 1
+        if i >= len(operations):
+            break
+    return five_executed
 
 
 def printing_data():
